fd2f1aa86461d4fa9e68b3b9a9bf48982616bebc
André Wagner <andre.nho@gmail.com>
2019-03-02 15:12:20 -0300

    .

--- terraform/dynamotable.tf
+++ terraform/dynamotable.tf
@@ -3,7 +3,7 @@ resource "aws_dynamodb_table" "artifacts" {
 	billing_mode 		= "PROVISIONED"
 	read_capacity 	= 2
 	write_capacity 	= 2
-	hash_key        = "Artifact"
+	hash_key        = "artifact"
 
 	attribute {
 		name = "artifact"
